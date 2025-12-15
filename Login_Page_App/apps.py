25429631949b264dd6111a0caa3df50ef6db812c
ChanikyaPonduri <chanikyaponduri22@gmail.com>
2025-12-15 23:25:07 +0530

    Initial Django login and signup system

---
+++ Login_Page_App/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LoginPageAppConfig(AppConfig):
+    name = 'Login_Page_App'
---
+++ Login_Page_App/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if User.objects.filter(username=username).exists():
+            return render(request, 'signup.html', {'error': 'Username already exists'})
+        else:
+            User.objects.create_user(username=username, password=password)
+            return redirect('login')
+    return render(request, 'signup.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    return render(request, 'login.html')
+
+
+@login_required(login_url='login')
+def home_view(request):
+    return render(request, 'home.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
